91594e4c3d481ff6a2d9f0516e65b020d42bcb07
GitHub Action <action@github.com>
2020-12-02 04:30:44 +0000

    Template cleanup

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,1 @@
-rootProject.name = "IntelliJ Platform Plugin Template"
+rootProject.name = "android_studio_template"
---
+++ src/main/kotlin/com/github/macvillanda/androidstudiotemplate/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.macvillanda.androidstudiotemplate.services
+
+import com.github.macvillanda.androidstudiotemplate.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
